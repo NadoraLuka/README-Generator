e11b59c3ef800f8b451762175f658717b1de74aa
NadoraLuka <69468234+NadoraLuka@users.noreply.github.com>
2020-09-15 22:02:47 -0400

    second commit

--- Develop/index.js
+++ Develop/index.js
@@ -1,7 +1,7 @@
 const fs = require("fs");
 const { prompt } = require("inquirer");
 const path= require("path");
-const generateMarkdown= require("./generateMarkdown");
+const generateMarkdown= require("./utils/generateMarkdown");
 
 // question to generate README
 const question = [
@@ -37,12 +37,26 @@ console.log('PLEASE BEGIN BY ANSWERING THE FIRST QUESTION');
 
 // function to write README file
 function writeToFile(fileName, data) {
-    return fs.writeFileSync(); 
+    return fs.writeFileSync(fileName,data); 
 }
 
 // function to initialize program
+// loop over questions
+// use inquirer to ask questions
+// write to README.md
 function init() {
-    writeToFile();
+    var text = "";
+    var i;
+for (i = 0; i < questions.length; i++) {
+  text += questions[i].name + ", ";
+   text += questions[i].message + ", ";
+   if (questions[i].type=="checkbox"){
+   		for (j= 0; j < questions[i].choices.length; j++){
+        }
+   }
+   
+}
+    //writeToFile();
 }
 
 // function call to initialize program
